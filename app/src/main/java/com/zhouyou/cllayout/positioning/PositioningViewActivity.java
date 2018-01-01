15f697f69c09766f36b6203fd5d1ad0ab441148a
kaka252 <kaka252@sina.com>
2018-01-01 15:30:47 +0800

    添加类

---
+++ app/src/main/java/com/zhouyou/cllayout/positioning/PositioningViewActivity.java
@@ -0,0 +1,17 @@
+package com.zhouyou.cllayout.positioning;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhouyou on 18/1/1.
+ */
+
+public class PositioningViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
